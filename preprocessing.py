8f35b4e83e1f88c90a72fdca5e3e1d34d133ce20
saiprakashreddyc@gmail.com <saiprakashreddyc@gmail.com>
2018-12-18 22:44:39 -0500

    adding basic code

---
+++ preprocessing.py
@@ -0,0 +1,46 @@
+from matplotlib.image import imread
+import glob   
+import pandas as pd
+import os
+import csv
+from collections import defaultdict
+
+def preprocessing():
+    columns = defaultdict(list) # each value in each column is appended to a list
+
+    with open('/Users/loubnabaroudi/Documents/UMICH/ME599/HW4_perception_solution/labels3.csv') as f:
+        reader = csv.DictReader(f) # read rows into a dictionary format
+        for row in reader: # read a row as {column1: value1, column2: value2,...}
+            for (k,v) in row.items(): # go over each column name and value 
+                columns[k].append(v)
+                
+    path_train = '/Users/loubnabaroudi/Documents/UMICH/ME599/HW4_perception_solution/deploy/trainval/*/*.jpg'   
+    files_train =glob.glob(path_train)
+
+    path_test = '/Users/loubnabaroudi/Documents/UMICH/ME599/HW4_perception_solution/deploy/test/*/*.jpg'   
+    files_test =glob.glob(path_test)
+    print(len(files_test))
+
+    image_train=[]
+    labels=[]
+    image_test=[]
+    for i in range(len(files_train)):
+        img=imread(files_train[i])
+        image_train.append(img)
+
+        l2 = list(files_train[i].split('/')[0:-1])
+        l3=list(os.path.split(os.path.abspath(files_train[i])))
+        l4=list(l3[1].split('_')[0:-1])
+        image_name=str(l2[-1]+'/'+l4[0])
+        index_label=columns["guid/image"].index(image_name)
+        list_label=columns["label"]
+
+        labels.append(list_label[index_label])
+
+    for i in range(len(files_test)):
+        img=imread(files_test[i])
+        image_test.append(img)
+
+    return (labels, image_train, image_test)
+ 
+labels,image_train,image_test=preprocessing()
---
+++ readme.md
@@ -0,0 +1,8 @@
+This is made as part of course project for Self Driving Cars at umich. 
+
+The data for this can be downloaded at https://www.kaggle.com/c/fall2018-rob535-task1/data
+
+Github repository : 
+
+To clone : 
+
